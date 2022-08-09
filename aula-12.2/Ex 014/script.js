7f981b6074292c79da571b268a047ebf970dab7e
Alevazdemello <alexandrevictor.vazdemello@gmail.com>
2022-08-08 21:00:39 -0300

    Atualizacao projeto

--- aula-12.2/Ex 014/script.js
+++ aula-12.2/Ex 014/script.js
@@ -1,17 +1,20 @@
 function carregar() {
   var msg = window.document.getElementById('msg')
-  var img = window.document.getElementById('igm')
+  var img = window.document.getElementById('img')
   var data = new Date()
   var hora = data.getHours()
   msg.innerHTML = `Agora são ${hora} horas.`
   if (hora >= 5 && hora < 12) {
     img.src = 'foto-manha.jpg'
+    document.body.style.background = '#676c78'
     //bomdia
   } else if (hora >= 12 && hora < 18) {
     img.src = 'foto-tarde.jpg'
+    document.body.style.background = '#FF4500'
     //boatarde
   } else {
     img.src = 'foto-noite.jpg'
+    document.body.style.background = '#8A2BE2'
     //boanoite
   }
 }
