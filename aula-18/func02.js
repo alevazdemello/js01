44359e652771f21e849a5291d5d4cd25e6404213
Alevazdemello <alexandrevictor.vazdemello@gmail.com>
2022-09-07 23:42:18 -0300

    Funcoes JS

---
+++ aula-18/func02.js
@@ -0,0 +1,18 @@
+function soma(n1 = 0, n2 = 0) {
+  return n1 + n2
+}
+
+console.log(soma(7))
+
+function somaDois(n1 = 0, n2 = 0) {
+  return n1 + n2
+}
+
+console.log(somaDois(2, 2))
+
+
+let v = function (x) {
+  return x * 2
+}
+
+console.log(v(5))
