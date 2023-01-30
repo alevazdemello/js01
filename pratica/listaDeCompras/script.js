0895a65fde8f48d5ffb3d3330beec981ac9158e8
Alexandre Vaz de Mello <102325671+alevazdemello@users.noreply.github.com>
2023-01-30 17:27:01 -0300

    Create script.js

---
+++ pratica/listaDeCompras/script.js
@@ -0,0 +1,31 @@
+var numProdutos = 0;
+var valTotal = 0;
+
+var resposta = "";
+
+function registro() {
+    inProduto = document.getElementById("inProduto");
+    inValor = document.getElementById("inValor");
+    valor = Number(inValor.value);
+    produto = inProduto.value;
+
+    if (produto == "" || valor == "" || isNaN(valor)) {
+        alert("Informe os dados corretamente.")
+        inProduto.focus();
+        return;
+    }
+
+    numProdutos++
+    valTotal = valTotal + valor;
+    resposta = resposta + produto + " - R$: " + valor.toFixed(2) + "\n";
+
+    document.getElementById("outRes").textContent = resposta + "---------------";
+
+    outTotal.textContent = numProdutos + " Produto(s) - Total R$ " + valTotal.toFixed(2);
+
+    inProduto.value = "";
+    inValor.value = "";
+    inProduto.focus();
+}
+
+btRegistrar.addEventListener("click", registro);
