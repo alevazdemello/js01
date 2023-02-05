78bf5a30093d1a0e88567192adfd1e7844ada01b
Alexandre Vaz de Mello <102325671+alevazdemello@users.noreply.github.com>
2023-02-05 20:11:32 -0300

    Update script.js

--- pratica/progUltimasNoticias/script.js
+++ pratica/progUltimasNoticias/script.js
@@ -4,7 +4,7 @@ function adicionar() {
     var inNews = document.getElementById("inNews");
     var noticias = inNews.value;
 
-    if(noticias == "") {
+    if (noticias == "") {
         alert("Informe os dados corretamente");
         inNews.focus();
         return;
@@ -14,8 +14,35 @@ function adicionar() {
     inNews.value = "";
     inNews.focus();
 
-  document.getElementById("outRes2").textContent = listaNews;
+    document.getElementById("outRes").textContent = "Notícias cadastradas: " + listaNews.length;
 }
 
 btAdd.addEventListener("click", adicionar);
 
+
+function listarUltimasNoticias() {
+    var quant = Number(prompt("Qual número de notícias você deseja visualizar?"));
+
+    var total = listaNews.length;
+
+    if (quant == 0 || isNaN(quant) || quant > total) {
+        alert("Número inválido");
+        return;
+    }
+
+
+    var ultimas = quant + " Últimas notícias \n-----------------------------\n";
+
+
+    for (var i = total - 1; i >= total - quant; i--) {
+        ultimas += (i + 1) + "°) " + listaNews[i] + "\n";
+    }
+
+
+    document.getElementById("outRes2").textContent = ultimas;
+
+
+
+}
+
+btListar.addEventListener("click", listarUltimasNoticias);
